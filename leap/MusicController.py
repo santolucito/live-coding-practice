35ee0c95ceac7e7373377b1c3353ab67efd137c6
Mark Santolucito <mark.santolucito@yale.edu>
2018-11-16 15:58:05 -0500

    startin with the leap

---
+++ leap/MusicController.py
@@ -0,0 +1,156 @@
+################################################################################
+# Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               #
+# Leap Motion proprietary and confidential. Not for distribution.              #
+# Use subject to the terms of the Leap Motion SDK Agreement available at       #
+# https://developer.leapmotion.com/sdk_agreement, or another agreement         #
+# between Leap Motion and you, your company or other organization.             #
+################################################################################
+
+import sys, thread, time
+sys.path.append("/usr/lib/Leap")
+sys.path.append("../lib/")
+sys.path.append("../lib/x64")
+import Leap
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+import OSC
+
+ampState = 0
+ampStateSetAt = 0
+
+class SampleListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+    osc_client = None
+
+    def on_init(self, controller):
+        print "Initialized"
+
+    def add_osc_connect(self, client_connection):
+        self.osc_client = client_connection
+        print (self.osc_client)
+
+    def on_connect(self, controller):
+        print "Connected"
+
+        # Enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+
+#        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+              #frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+
+        # Get tools
+        for tool in frame.tools:
+
+            print "  Tool id: %d, position: %s, direction: %s" % (
+                tool.id, tool.tip_position, tool.direction)
+
+        for hand in frame.hands:
+            msg = OSC.OSCMessage()
+            msg.setAddress("/pinch")
+            msg.append(hand.pinch_strength)
+            self.osc_client.send(msg)
+            print(hand.pinch_strength)
+            
+
+        # Get gestures
+        for gesture in frame.gestures():
+            if gesture.type == Leap.Gesture.TYPE_CIRCLE:
+                circle = CircleGesture(gesture)
+
+                # Determine clock direction using the angle between the pointable and the circle normal
+                if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
+                    clockwiseness = "clockwise"
+                else:
+                    clockwiseness = "counterclockwise"
+
+            if gesture.type == Leap.Gesture.TYPE_SWIPE:
+                swipe = SwipeGesture(gesture)
+                msg = OSC.OSCMessage()
+                msg.setAddress("/swipeSpeed")
+                msg.append(swipe.speed)
+                self.osc_client.send(msg)
+                print "  Swipe id: %d, state: %s, position: %s, direction: %s, speed: %f" % (
+                        gesture.id, self.state_names[gesture.state],
+                        swipe.position, swipe.direction, swipe.speed)
+            
+            if gesture.type == Leap.Gesture.TYPE_KEY_TAP:
+                keytap = KeyTapGesture(gesture)
+                msg = OSC.OSCMessage()
+                msg.setAddress("/keytap")
+                global ampState
+                global ampStateSetAt
+                if (frame.timestamp - ampStateSetAt> 1000000):
+                    ampStateSetAt = frame.timestamp
+                    ampState = int(not ampState)
+                    msg.append(ampState)
+                    self.osc_client.send(msg)
+                print "  Key Tap id: %d, %s, position: %s, direction: %s" % (
+                        gesture.id, self.state_names[gesture.state],
+                        keytap.position, keytap.direction )
+
+            if gesture.type == Leap.Gesture.TYPE_SCREEN_TAP:
+                screentap = ScreenTapGesture(gesture)
+                print "  Screen Tap id: %d, %s, position: %s, direction: %s" % (
+                        gesture.id, self.state_names[gesture.state],
+                        screentap.position, screentap.direction )
+            
+
+        #if not (frame.hands.is_empty and frame.gestures().is_empty):
+            #print ""
+
+    def state_string(self, state):
+        if state == Leap.Gesture.STATE_START:
+            return "STATE_START"
+
+        if state == Leap.Gesture.STATE_UPDATE:
+            return "STATE_UPDATE"
+
+        if state == Leap.Gesture.STATE_STOP:
+            return "STATE_STOP"
+
+        if state == Leap.Gesture.STATE_INVALID:
+            return "STATE_INVALID"
+
+def main():
+    # Setup a connection between SC and Py wtih OSC
+    c = OSC.OSCClient()
+    c.connect(('127.0.0.1', 57120))
+
+    # Create a sample listener and controller
+    listener = SampleListener()
+    listener.add_osc_connect(c)
+
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
